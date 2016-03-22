a24000f40cb74683079f974ef505dec375c5041d
Filip Sochal <fsochal@gmail.com>
2016-03-22 19:01:56 +0100

    Initial commit. Includes project created with Maven.

---
+++ src/main/java/diaballik/DiaballikApplication.java
@@ -0,0 +1,13 @@
+package diaballik;
+
+/**
+ * Hello world!
+ *
+ */
+public class DiaballikApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
