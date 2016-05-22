c1785d01e4c18c756af37d3f9d297abca7fcf854
Amai Rafał <kuroamai@gmail.com>
2016-05-23 01:08:27 +0200

    added (a very very) simple GUI

--- src/main/java/diaballik/Diaballik.java
+++ src/main/java/diaballik/Diaballik.java
@@ -1,10 +1,14 @@
 package diaballik;
 
 import diaballik.gui.TextualUI;
+import diaballik.gui.GraphicalUI;
 
 public class Diaballik  {  
     public static void main(String[] args) {
-        TextualUI ui = new TextualUI();
-        ui.initializeUI();
+        //TextualUI ui = new TextualUI();
+        //ui.initializeUI();
+        
+        GraphicalUI ui = new GraphicalUI();
+        ui.pseudoMain();
     }
 }
